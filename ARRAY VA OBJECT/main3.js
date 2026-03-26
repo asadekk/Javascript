dceb04abb0478934f6c504d33a506e0476e1a6da
asadekk <asadesd21@gmail.com>
2026-03-26 22:12:18 +0500

    object

--- ARRAY VA OBJECT/main3.js
+++ ARRAY VA OBJECT/main3.js
@@ -1,9 +1,47 @@
 
-const ismlar = ["asadbek" , "too", "luffy"];
-const newI = [];
+// const ismlar = ["asadbek" , "too", "luffy"];
+// const newI = [];
 
-ismlar.forEach(function (ism) {
-    let newism = ism.charAt().toUpperCase()+ism.slice(1)+"bek"
-    newI.push(newism)
-})
-console.log(newI);
+// ismlar.forEach(function (ism) {
+//     let newism = ism.charAt().toUpperCase()+ism.slice(1)+"bek"
+//     newI.push(newism)
+// })
+// console.log(newI);
+
+// let array = [1,23,4,45,6,4]
+
+// array.forEach(function (n) {
+//     console.log(n);
+    
+// })
+
+let person = {
+    name : "Asadbek",
+    age : 16,
+    bestAnime : "ONE PIES",
+    mail : "zoro@gmail.com",
+    phone : "Redmi 5",
+    endAnime : function () {
+        console.log('Siz hali onepisni tugatmadingiz')
+        
+    },
+    Anime : function () {
+        console.log('Elita clasroom')
+        
+    }
+}
+
+console.log(this);
+
+person.Anime()
+person.endAnime()
+console.log(person);
+console.log(person.name);
+console.log(person["bestAnime"]);
+
+
+let colors = ["red", "blue", "green"];
+
+let randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+console.log(randomColor);
